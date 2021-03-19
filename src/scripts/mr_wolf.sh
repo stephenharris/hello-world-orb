8f4b3f37b26633b8e39e43e4eda2391f0aeba2fe
Stephen Harris <contact@stephenharris.info>
2021-03-19 20:07:29 +0000

    add command

---
+++ src/scripts/mr_wolf.sh
@@ -0,0 +1,8 @@
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    echo "date is $(date)"
+    export MR_WOLF=$(date)
+fi
