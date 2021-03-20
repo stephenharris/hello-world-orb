3cdfb71b49907777cad131c59cba7a3c82447d7e
Stephen Harris <contact@stephenharris.info>
2021-03-20 22:44:20 +0000

    fix job

--- src/scripts/greetpy.sh
+++ src/scripts/greetpy.sh
@@ -2,4 +2,4 @@ cat >/tmp/cluster_check.py <<"EOF"
 include cluster_check.py
 EOF
 
-python3 /tmp/cluster_check.py --wait-for=<<parameters.wait-for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
+python3 /tmp/cluster_check.py --wait-for=<<parameters.wait_for>> --application=<<parameters.application>> --target=<<parameters.target>> --argocd-url <<parameters.argocd_url>>
